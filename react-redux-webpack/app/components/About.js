14d2c7ba2adad6681a55ed9dfa8eca121d386c94
Lucky <175917645@qq.com>
2017-02-28 23:03:38 +0800

    add About and App components

---
+++ react-redux-webpack/app/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () =>
+  <div>
+    About page
+    <div>
+      <iframe src="http://www.softlucky.com" frameBorder="0" allowTransparency="true" scrolling="0"></iframe>
+    </div>
+  </div>;
+
+export default About;
