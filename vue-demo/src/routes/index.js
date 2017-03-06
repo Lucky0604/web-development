9b637b315a5df513de80ecad13634f532fdb5dd6
Lucky <175917645@qq.com>
2017-03-07 00:04:34 +0800

    add breadcrumb and pagination components

---
+++ vue-demo/src/routes/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import routesMap from './map/';       // 路由映射
+import hooks from './hooks/';       //路由钩子
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  // root: null,
+  // hashbang: false,
+  // history: true,
+  // saveScrollPosition: true,
+  // transitionOnLoad: true,
+  // linkActiveClass: '',
+  suppressTransitionError: __PROD__       // 生产环境下不抛出异常
+})
+
+router.map(routesMap);
+router.alias({'/msg': '/msg/list'});
+
+hooks(router);
+
+/**
+ * 调用router.start(App, '#app')后，根组件实例就会暴露到router.app
+ * 组件内部可通过this.$root访问，外部则通过router.app访问
+ */
+export default router;
