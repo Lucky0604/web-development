7581ca848a4e7d33af652c2e4828b30b09a28841
Lucky <175917645@qq.com>
2017-04-06 18:19:21 +0800

    add charge view

---
+++ admin/src/pages/Charge/RentManagement/RentQuery.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-tabs v-model="activeName2" type="card" @tab-click="handleClick">
+    <el-tab-pane label="待收租" name="first">待收租</el-tab-pane>
+    <el-tab-pane label="审核中" name="second">审核中</el-tab-pane>
+    <el-tab-pane label="审核失败" name="third">审核失败</el-tab-pane>
+    <el-tab-pane label="审核成功" name="fourth">审核成功</el-tab-pane>
+  </el-tabs>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      activeName2: 'second'
+    }
+  },
+  methods: {
+    handleClick(tab, event) {
+      
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -2,7 +2,7 @@
  * @Author: lucky
  * @Date:   2017-04-06T12:16:35+08:00
  * @Last modified by:   lucky
- * @Last modified time: 2017-04-06T15:44:39+08:00
+ * @Last modified time: 2017-04-06T18:14:49+08:00
  */
 
 
@@ -11,13 +11,31 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 
+// 收费管理
+import RentQuery from '@/pages/Charge/RentManagement/RentQuery'
+
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      component: Home
+      component: Home,
+      children: [
+        {
+          path: '/',
+          components: {
+            ContainerView: RentQuery
+          }
+        },
+        // 收费管理
+        {
+          path: '/RentQuery',
+          components: {
+            ContainerView: RentQuery
+          }
+        }
+      ]
     }
   ]
 })
