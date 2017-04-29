507eff4365218efec331a35b8f168c50fbc820e8
Lucky <175917645@qq.com>
2017-04-29 22:12:57 +0800

    add admin service and base api methods

---
+++ cms/backend/admin/article.js
@@ -0,0 +1,12 @@
+/**
+ * @Author: lucky
+ * @Date:   2017-04-29T18:52:48+08:00
+ * @Last modified by:   lucky
+ * @Last modified time: 2017-04-29T19:16:40+08:00
+ */
+
+
+
+import $ from '../utils'
+import Models from '../models'
+import Base from './base'
